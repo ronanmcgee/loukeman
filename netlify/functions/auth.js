147ffafe332798f231b9364f92b866150eaa3b9d
Ronan McGee <ronan@Ronans-MacBook-Pro-2.local>
2025-07-09 14:05:57 -0400

    Add serverless GitHub auth function

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -1,43 +1,30 @@
 const fetch = require("node-fetch");
 
 exports.handler = async function (event, context) {
-  try {
-    const code = event.queryStringParameters.code;
-    const client_id = process.env.GITHUB_CLIENT_ID;
-    const client_secret = process.env.GITHUB_CLIENT_SECRET;
+  const code = event.queryStringParameters.code;
 
-    if (!code) {
-      console.error("Missing code in query params");
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: "Missing code" }),
-      };
-    }
-
-    const response = await fetch("https://github.com/login/oauth/access_token", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ client_id, client_secret, code }),
-    });
-
-    const data = await response.json();
-
-    if (data.error) {
-      console.error("GitHub error:", data.error, data.error_description);
-    }
-
+  if (!code) {
     return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
-  } catch (err) {
-    console.error("Unexpected error:", err);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Internal server error" }),
+      statusCode: 400,
+      body: JSON.stringify({ error: "Missing code in query params" }),
     };
   }
+
+  const client_id = process.env.GITHUB_CLIENT_ID;
+  const client_secret = process.env.GITHUB_CLIENT_SECRET;
+
+  const response = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ client_id, client_secret, code }),
+  });
+
+  const data = await response.json();
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
 };
